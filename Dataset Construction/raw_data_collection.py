5113c22df4846dd5816b20025a949125af872520
morteza24 <mortezia4444.mz@gmail.com>
2024-09-09 01:28:19 +0330

    added raw_data_collection script

---
+++ Dataset Construction/raw_data_collection.py
@@ -0,0 +1,46 @@
+from datetime import datetime
+from threading import Thread
+import requests as rq
+from time import sleep
+import json
+
+
+def get_data(year, cursor):
+  count = 0
+  while cursor:
+    url = f"https://api.openalex.org/works?page=1&filter=type:types/article,publication_year:{year},language:languages/en&per_page=200&select=concepts&cursor={cursor}"
+    res = rq.get(url)
+    sleep(1)
+    if res.status_code != 200:
+      raise Exception(res.status_code)
+    d = res.json()
+    l = []
+    for result in d["results"]:
+      ids = []
+      for concept in result["concepts"]:
+        ids.append(concept["id"][21:])
+      l.append(ids)
+    cursor = d["meta"]["next_cursor"]
+    count += 1
+    if count == 100:
+      file_name = f"{year}_{str(datetime.now())}_{cursor}.json"
+      with open("./"+file_name, "wt") as f:
+        json.dump(l, f, separators=(',', ':'))
+      break
+
+start_year = 2010
+end_year = 2020
+
+years = list(range(start_year, end_year+1))
+threads = []
+
+for i, year in enumerate(years):
+  new_t = Thread(target=get_data, args=[year, "*"])
+  threads.append(new_t)
+  print("starting thread")
+  new_t.start()
+  if (i+1) % 10 == 0:
+    for t in threads:
+      t.join()
+    threads = []
+
