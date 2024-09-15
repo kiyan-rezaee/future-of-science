d131dcce3b41489e13bed90afa4d7bb491d43093
morteza24 <mortezia4444.mz@gmail.com>
2024-09-15 23:32:45 +0330

    edited TrendDatabase.py

--- TrendDatabase.py
+++ TrendDatabase.py
@@ -1,63 +1,34 @@
-import psycopg2
-import json
 from itertools import combinations
-import os
-import glob
-
-def find_json_files(folder_path):
-    json_files = glob.glob(os.path.join(folder_path, '**', '*.json'), recursive=True)
-    return json_files
-
-folder_path = './'
-
-json_files = find_json_files(folder_path)
-
-conn = psycopg2.connect(
-    host="your_host",
-    database="your_database",
-    user="your_username",
-    password="your_password"
-)
-cur = conn.cursor()
-
-cur.execute('''
-    CREATE TABLE IF NOT EXISTS concept_pairs (
-        Concept1 VARCHAR(255),
-        Concept2 VARCHAR(255),
-        year INTEGER,
-        PRIMARY KEY (Concept1, Concept2, year)
-    )
-''')
-conn.commit()
-
-def load_existing_pairs(year):
-    cur.execute('''
-        SELECT Concept1, Concept2 FROM concept_pairs WHERE year = %s
-    ''', (year,))
-    existing_pairs = cur.fetchall()
-    return set((min(c1, c2), max(c1, c2)) for c1, c2 in existing_pairs)
+import os, glob
+import psycopg
+import json
 
 
-def insert_concept_pairs(concept_list, year, existing_pairs):
-    for concept1, concept2 in combinations(sorted(concept_list), 2):
-        pair = (concept1, concept2)
-        if pair not in existing_pairs:
-            cur.execute('''
-                INSERT INTO concept_pairs (Concept1, Concept2, year)
-                VALUES (%s, %s, %s)
-            ''', (concept1, concept2, year))
-            existing_pairs.add(pair)
-            conn.commit()
+folder_path = "json_files"
+json_files = glob.glob(os.path.join(folder_path, '**', '*.json'), recursive=True)
+len_files = len(json_files)
 
 
+with psycopg.connect("host=localhost dbname=TrendDB user=Trend password=2424") as conn:
+    with conn.cursor() as cur:
+        cur.execute("""
+            CREATE TABLE IF NOT EXISTS concept_pairs(
+                Concept1 VARCHAR(255) NOT NULL,
+                Concept2 VARCHAR(255) NOT NULL,
+                year INTEGER,
+                PRIMARY KEY (Concept1, Concept2, year))
+            """)
+        for i, json_file in enumerate(json_files):
+            year = int(json_file.split("/")[-1][:4])
+            with open(json_file, 'rt') as f:
+                data = json.load(f)
+            len_data = len(data)
+            for j, concept_group in enumerate(data):
+                for concept1, concept2 in combinations(sorted(concept_group), 2):
+                    cur.execute(f"INSERT INTO concept_pairs VALUES ('{concept1}', '{concept2}', {year}) ON CONFLICT DO NOTHING")
+                    conn.commit()
+                if (j+1) % 10 == 0:
+                    print(f"{j+1}/{len_data}")
+            print(f"file {i+1}/{len_files} finished:", json_file)
+            print()
 
-for json_file in json_files:
-    year = int(json_file[2:6])
-    with open(json_file, 'r') as f:
-        data = json.load(f)
-    for concept_group in data:
-        insert_concept_pairs(concept_group, year)
-    print(f"{json_file} is done!")    
-
-cur.close()
-conn.close()
